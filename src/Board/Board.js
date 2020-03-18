4bc9818f44b5f8a24c66075e5fb1e3e67113aef4
haileyli <hailey@Haileys-MacBook-Air-2.local>
2020-03-18 17:11:37 -0400

    update

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -41,69 +41,80 @@ class Board extends Component {
         };
     }
 
-    randomGenTwice(){
+    randomGenTwice() {
         //change a random 0 to 2/4
         let a = [];
         let b = [];
         let temp_d = this.state.data;
-        for (var i = 0; i < temp_d.length; i++){
-            for (var j = 0; j < temp_d[0].length; j++){
+        for (var i = 0; i < temp_d.length; i++) {
+            for (var j = 0; j < temp_d[0].length; j++) {
                 console.log(temp_d[i][j]);
-                if (temp_d[i][j] === 0){
+                if (temp_d[i][j] === 0) {
                     a.push(i);
                     b.push(j);
                 }
             }
         }
-        if (a.length == 0){
+        if (a.length == 0) {
             return;
-        } else if (a.length == 1){
+        } else if (a.length == 1) {
             temp_d[a[0]][b[0]] = 2;
-            this.setState({data: temp_d});
+            this.setState({ data: temp_d });
         } else {    //a.length >= 2
             let ranIndex1 = getRandomInt(a.length);
             let ranIndex2 = getRandomInt(a.length);
-            if (ranIndex2 === ranIndex1){
+            if (ranIndex2 === ranIndex1) {
                 ranIndex2 = getRandomInt(a.length);
             }
             temp_d[a[ranIndex1]][b[ranIndex1]] = 2;
             temp_d[a[ranIndex2]][b[ranIndex2]] = 2;
-            this.setState({data: temp_d});
+            this.setState({ data: temp_d });
         }
-        
+
         //this.forceUpdate();
     }
+    componentDidMount() {
+        document.addEventListener('keydown', this.onKeyDown.bind(this));
+    }
 
-    randomGenOnce(){
+    onKeyDown = (e) => {
+        e.preventDefault();
+        if (e.which === 33) {
+            console.log("hi")
+        }
+    }
+
+
+    randomGenOnce() {
         //change a random 0 to 2/4
         let a = [];
         let b = [];
         let temp_d = this.state.data;
-        for (var i = 0; i < temp_d.length; i++){
-            for (var j = 0; j < temp_d[0].length; j++){
+        for (var i = 0; i < temp_d.length; i++) {
+            for (var j = 0; j < temp_d[0].length; j++) {
                 console.log(temp_d[i][j]);
-                if (temp_d[i][j] === 0){
+                if (temp_d[i][j] === 0) {
                     a.push(i);
                     b.push(j);
                 }
             }
         }
-        if (a.length == 0){
+        if (a.length == 0) {
             return;
         }
         let ranIndex = getRandomInt(a.length);
         temp_d[a[ranIndex]][b[ranIndex]] = 2;
-        this.setState({data: temp_d});
-        this.setState({genCount: this.state.genCount+1});
+        this.setState({ data: temp_d });
+        this.setState({ genCount: this.state.genCount + 1 });
         //this.forceUpdate();
     }
 
     render() {
-        
+
         return (
 
             <div className="board">
-                <button onClick={()=>{this.randomGenTwice()}}>RandomGen</button>
+                <button onClick={() => { this.randomGenTwice() }}>RandomGen</button>
                 <div className="grid-container" >
                     <div className="grid-row" >
                         <div className="grid-cell" ></div>
@@ -131,30 +142,14 @@ class Board extends Component {
                 </div>
 
                 <div className="tile-container">
-
-
-
                     {
-
                         this.state.data.map((item, i) => {
-                            return <div>
-                                {item.map((it, j) => {
-                                    if (it !== 0)
-                                        return <div className={"tile tile-" + it + " tile-position-" + (i + 1) + "-" + (j + 1)}>
-                                            <div className="tile-inner">{it}</div></div>
-                                })}
-                            </div>
-
-
+                            return <div>{item.map((it, j) => {
+                                if (it !== 0) return <div className={"tile tile-" + it + " tile-position-" + (i + 1) + "-" + (j + 1)} >
+                                    <div className="tile-inner">{it}</div></div>
+                            })}</div>
                         })
-
                     }
-
-
-
-
-
-
                 </div>
 
             </div>
