04d16a5662de4688905f61f96edcb4056de8a373
Brian <1219607233@qq.com>
2020-03-18 17:08:33 -0400

    random gen once/twice

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import './Board.css';
 
-
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
 class Board extends Component {
     constructor(props) {
@@ -33,18 +35,75 @@ class Board extends Component {
                     0,
                     4
                 ]
-            ]
+            ],
+            gen: true,
 
         };
     }
 
+    randomGenTwice(){
+        //change a random 0 to 2/4
+        let a = [];
+        let b = [];
+        let temp_d = this.state.data;
+        for (var i = 0; i < temp_d.length; i++){
+            for (var j = 0; j < temp_d[0].length; j++){
+                console.log(temp_d[i][j]);
+                if (temp_d[i][j] === 0){
+                    a.push(i);
+                    b.push(j);
+                }
+            }
+        }
+        if (a.length == 0){
+            return;
+        } else if (a.length == 1){
+            temp_d[a[0]][b[0]] = 2;
+            this.setState({data: temp_d});
+        } else {    //a.length >= 2
+            let ranIndex1 = getRandomInt(a.length);
+            let ranIndex2 = getRandomInt(a.length);
+            if (ranIndex2 === ranIndex1){
+                ranIndex2 = getRandomInt(a.length);
+            }
+            temp_d[a[ranIndex1]][b[ranIndex1]] = 2;
+            temp_d[a[ranIndex2]][b[ranIndex2]] = 2;
+            this.setState({data: temp_d});
+        }
+        
+        //this.forceUpdate();
+    }
 
+    randomGenOnce(){
+        //change a random 0 to 2/4
+        let a = [];
+        let b = [];
+        let temp_d = this.state.data;
+        for (var i = 0; i < temp_d.length; i++){
+            for (var j = 0; j < temp_d[0].length; j++){
+                console.log(temp_d[i][j]);
+                if (temp_d[i][j] === 0){
+                    a.push(i);
+                    b.push(j);
+                }
+            }
+        }
+        if (a.length == 0){
+            return;
+        }
+        let ranIndex = getRandomInt(a.length);
+        temp_d[a[ranIndex]][b[ranIndex]] = 2;
+        this.setState({data: temp_d});
+        this.setState({genCount: this.state.genCount+1});
+        //this.forceUpdate();
+    }
 
     render() {
-
+        
         return (
 
             <div className="board">
+                <button onClick={()=>{this.randomGenTwice()}}>RandomGen</button>
                 <div className="grid-container" >
                     <div className="grid-row" >
                         <div className="grid-cell" ></div>
