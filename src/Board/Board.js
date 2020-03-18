637afc89f29cc264443f833920c632f00adae51b
haileyli <hailey@Haileys-MacBook-Air-2.local>
2020-03-18 16:00:34 -0400

    tiles

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -8,6 +8,32 @@ class Board extends Component {
         super(props);
 
         this.state = {
+            data: [
+                [
+                    2,
+                    4,
+                    0,
+                    8
+                ],
+                [
+                    2,
+                    2,
+                    0,
+                    0
+                ],
+                [
+                    64,
+                    2,
+                    0,
+                    0
+                ],
+                [
+                    2,
+                    2,
+                    0,
+                    0
+                ]
+            ]
 
         };
     }
@@ -19,31 +45,70 @@ class Board extends Component {
         return (
 
             <div className="board">
-                <div class="grid-container" >
-                    <div class="grid-row" >
-                        <div class="grid-cell" ></div>
-                        <div class="grid-cell" ></div>
-                        <div class="grid-cell" ></div>
-                        <div class="grid-cell" ></div>
-                    </div><div class="grid-row" >
-                        <div class="grid-cell" ></div>
-                        <div class="grid-cell" ></div>
-                        <div class="grid-cell" ></div>
-                        <div class="grid-cell"></div>
+                <div className="grid-container" >
+                    <div className="grid-row" >
+                        <div className="grid-cell" ></div>
+                        <div className="grid-cell" ></div>
+                        <div className="grid-cell" ></div>
+                        <div className="grid-cell" ></div>
+                    </div><div className="grid-row" >
+                        <div className="grid-cell" ></div>
+                        <div className="grid-cell" ></div>
+                        <div className="grid-cell" ></div>
+                        <div className="grid-cell"></div>
                     </div>
-                    <div class="grid-row" >
-                        <div class="grid-cell" ></div>
-                        <div class="grid-cell" ></div>
-                        <div class="grid-cell" ></div>
-                        <div class="grid-cell" ></div>
-                    </div><div class="grid-row">
-                        <div class="grid-cell" >
-                        </div><div class="grid-cell" >
-                        </div><div class="grid-cell" >
-                        </div><div class="grid-cell" >
+                    <div className="grid-row" >
+                        <div className="grid-cell" ></div>
+                        <div className="grid-cell" ></div>
+                        <div className="grid-cell" ></div>
+                        <div className="grid-cell" ></div>
+                    </div><div className="grid-row">
+                        <div className="grid-cell" >
+                        </div><div className="grid-cell" >
+                        </div><div className="grid-cell" >
+                        </div><div className="grid-cell" >
                         </div>
                     </div>
                 </div>
+
+                <div className="tile-container">
+
+
+
+                    {
+                        // for (var i = 0; i < this.state.data.length; i++){
+                        //     for (var j = 0; j < item.length; j++) {
+                        //         return <div className={"tile tile-" + item[j] + " tile-position-" + (i + 1) + "-" + (j + 1)}>
+                        //             <div className="tile-inner">{item[j]}</div></div>
+                        //     }
+                        // }
+
+
+
+
+                        this.state.data.map((item, i) => {
+                            //document.write(i)
+
+                            return <div><div className={"tile tile-" + item[0] + " tile-position-" + (i + 1) + "-" + (1)}>
+                                <div className="tile-inner">{item[0]}</div></div>
+                                <div className={"tile tile-" + item[1] + " tile-position-" + (i + 1) + "-" + (2)}>
+                                    <div className="tile-inner">{item[1]}</div></div>
+                                <div className={"tile tile-" + item[2] + " tile-position-" + (i + 1) + "-" + (3)}>
+                                    <div className="tile-inner">{item[2]}</div></div>
+                                <div className={"tile tile-" + item[3] + " tile-position-" + (i + 1) + "-" + (4)}>
+                                    <div className="tile-inner">{item[3]}</div></div></div>
+
+                        })
+
+                    }
+
+
+
+
+
+
+                </div>
+
             </div>
 
         );
