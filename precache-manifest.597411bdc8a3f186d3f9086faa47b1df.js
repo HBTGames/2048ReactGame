79325b64336492edf71941f1c5773880f571d921
haileyli <hailey@Haileys-MacBook-Air-2.local>
2020-03-20 17:06:12 -0400

    Updates

---
+++ precache-manifest.597411bdc8a3f186d3f9086faa47b1df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1efc12dd48bb73400db7c0c00d64db22",
+    "url": "/2048ReactGame/index.html"
+  },
+  {
+    "revision": "1dd55417866442f9dc48",
+    "url": "/2048ReactGame/static/css/main.6ee1c805.chunk.css"
+  },
+  {
+    "revision": "7adfc6737f349afdbe68",
+    "url": "/2048ReactGame/static/js/2.b7c22166.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/2048ReactGame/static/js/2.b7c22166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dd55417866442f9dc48",
+    "url": "/2048ReactGame/static/js/main.1f97b1ca.chunk.js"
+  },
+  {
+    "revision": "47312b3aa07c2e50385c",
+    "url": "/2048ReactGame/static/js/runtime-main.3c915df7.js"
+  }
+]);
